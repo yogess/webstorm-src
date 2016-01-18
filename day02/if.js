a690699290cf2780459bde0d60075c0a7b36ad5d
Yogess <zhouang9@163.com>
2016-01-18 20:31:27 +0800

    add one file.

---
+++ day02/if.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/1/18.
+ */
+var age = 3;
+if (age >= 18) {
+    alert('adult');
+} else if (age >= 6) {
+    alert('teenager');
+} else {
+    alert('kid');
+}
